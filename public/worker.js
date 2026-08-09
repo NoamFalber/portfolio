7f8b77245dff9fcef47ab98b058801873976453d
NoamFalber <93486845+NoamFalber@users.noreply.github.com>
2026-08-10 02:09:04 +0300

    Deploy final WebGPU project build

--- public/worker.js
+++ public/worker.js
@@ -2,15 +2,15 @@
 
 const unityBrotliTypes = new Map([
   [
-    "/builds/project-one/Build/web-build-v2.data.br",
+    "/builds/project-one/Build/game-one-webgpu-20260809.data.br",
     "application/octet-stream",
   ],
   [
-    "/builds/project-one/Build/web-build-v2.framework.js.br",
+    "/builds/project-one/Build/game-one-webgpu-20260809.framework.js.br",
     "application/javascript",
   ],
   [
-    "/builds/project-one/Build/web-build-v2.wasm.br",
+    "/builds/project-one/Build/game-one-webgpu-20260809.wasm.br",
     "application/wasm",
   ],
 ]);
@@ -20,7 +20,7 @@ export default {
     const assetResponse = await environment.ASSETS.fetch(request);
     const contentType = unityBrotliTypes.get(new URL(request.url).pathname);
 
-    if (!contentType || !assetResponse.body || assetResponse.status !== 200) {
+    if (!contentType || assetResponse.status !== 200) {
       return assetResponse;
     }
 
